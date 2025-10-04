fdb3599a1cf5465d7f6f8cab157847051a493c47
YarKhan02 <yarkhan025@gmail.com>
2025-10-05 02:59:35 +0500

    #1 create s3 bucket and terraform destory

--- main.tf
+++ main.tf
@@ -4,12 +4,28 @@ terraform {
       source = "hashicorp/random"
       version = "3.7.2"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.15.0"
+    }
   }
 }
 
+provider "aws" {
+  region  = "ap-southeast-2"
+  profile = "terratown"
+}
+
 resource "random_string" "bucket_name" {
-  length           = 16
-  special          = false
+  length  = 16
+  lower   = true
+  upper   = false
+  numeric = false
+  special = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
